e4c173be2ee7c1cb8aea41f13d030836232a6907
melniciucd <melniciucd@gmail.com>
2022-07-26 15:34:21 +0300

    changed version

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "author": "demania",
     "description": "",
     "blender": (2, 80, 0),
-    "version": (0, 0, 1),
+    "version": (0, 0, 2),
     "location": "",
     "warning": "",
     "category": "Generic",
