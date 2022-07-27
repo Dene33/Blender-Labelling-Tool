69eb35abdc2256087347003fc993b83d6acfe090
Dene <den33.info@gmail.com>
2022-07-27 21:57:10 +0300

    initial  pascal_voc, coco, yolo bounding boxes data to export

--- store_anim_data.py
+++ store_anim_data.py
@@ -4,6 +4,9 @@ import os
 
 os.system("cls")
 
+def pascal_voc_to_yolo(x_min, y_min, x_max, y_max, image_w, image_h):
+    return [((x_max + x_min) / 2) / image_w, ((y_max + y_min) / 2) / image_h, (x_max - x_min)/image_w, (y_max - y_min)/image_h]
+
 # ===========================================================
 
 frame_start = bpy.context.scene.frame_start
@@ -11,7 +14,7 @@ frame_end = bpy.context.scene.frame_end
 
 # ===========================================================
 
-arm = bpy.data.objects["dancer_00"]
+arm = bpy.data.objects["bb"]
 
 # ===========================================================
 
@@ -37,7 +40,12 @@ render_size = (
 
 for frame in range(frame_start, frame_end + 1):
     bpy.context.scene.frame_set(frame)
-
+        
+    bones_coords = {'top_left': (),
+                     'top_right': (),
+                     'bottom_left': (),
+                     'bottom_right': ()}
+    
     # print(frame)
     for bone in arm.pose.bones:
         arm_matrix = arm.matrix_world
@@ -46,20 +54,29 @@ for frame in range(frame_start, frame_end + 1):
         co_2d = bpy_extras.object_utils.world_to_camera_view(scene, cam, bone_world_head)
         
         co_2d_x = (round(co_2d.x * render_size[0]))
-        co_2d_y = (round(co_2d.y * render_size[1]))
-        
-        co_2d_x2 = (round(co_2d.x * render_size[0]))
-        co_2d_y2 = (render_size[1]-round(co_2d.y * render_size[1]))
-        
-        print(f" x  {co_2d_x}")
-        print(f" x2 {co_2d_x2}")
-        print(f" y  {co_2d_y}")
-        print(f" y2 {co_2d_y2}")
-        print()
 
+        co_2d_y = (render_size[1]-round(co_2d.y * render_size[1]))
+        
+        bones_coords[bone.name] = (co_2d_x, co_2d_y)
 
         bones_info[bone.name] += [[frame, co_2d_x, co_2d_y]]
         # bones_info[bone.name] += [[frame, bone.head, bone.tail]]
+    
+    print('pascal_voc')
+    print(f"x_min:{bones_coords['top_left'][0]}", f"y_min:{bones_coords['top_left'][1]}",
+          f"x_max:{bones_coords['bottom_right'][0]}", f"y_max:{bones_coords['bottom_right'][1]}")
+          
+    print('coco')
+    width = bones_coords['top_right'][0] - bones_coords['top_left'][0]
+    height = bones_coords['bottom_left'][1] - bones_coords['top_left'][1]
+    print(f"x_min:{bones_coords['top_left'][0]}", f"y_min:{bones_coords['top_left'][1]}",
+          f"width:{width}", f"height:{height}")
+          
+    print('yolo')
+    yolo_coords = pascal_voc_to_yolo(bones_coords['top_left'][0], bones_coords['top_left'][1],
+                                     bones_coords['bottom_right'][0], bones_coords['bottom_right'][1],
+                                     scene.render.resolution_x, scene.render.resolution_y)
+    print(f"x_center: {yolo_coords[0]}", f"y_center: {yolo_coords[1]}", f"width: {yolo_coords[2]}", f"height: {yolo_coords[3]}")
 
 
 # ===========================================================
